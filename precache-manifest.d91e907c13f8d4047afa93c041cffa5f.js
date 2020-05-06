e8619ac07db7cd237cf02fe7d941d46bc3574b83
Kalib Hackin (via Travis CI) <naguihw@gmail.com>
2020-05-06 03:31:44 +0000

    Deploy release

---
+++ precache-manifest.d91e907c13f8d4047afa93c041cffa5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918bb86065cb76da20934f08222f8e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "951e97448d0a2060b9a5",
+    "url": "/static/css/main.63f4aed2.chunk.css"
+  },
+  {
+    "revision": "979223241436a4dba8df",
+    "url": "/static/js/2.c8b63a26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8b63a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951e97448d0a2060b9a5",
+    "url": "/static/js/main.e635aa03.chunk.js"
+  },
+  {
+    "revision": "477bc82afd223627d4d3",
+    "url": "/static/js/runtime-main.d3255406.js"
+  },
+  {
+    "revision": "8275e5fe64efd82da36f1017a59fb4ed",
+    "url": "/static/media/sort-down-solid.8275e5fe.svg"
+  }
+]);
